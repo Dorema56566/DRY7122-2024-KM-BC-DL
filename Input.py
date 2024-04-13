e0872e6df6e89c2558d2cc6d9bb6f4be915dd788
Dorema56566 <166890600+Dorema56566@users.noreply.github.com>
2024-04-12 23:55:59 -0400

    DRY7122-2024-KM-BC-DL
    Kevin san Martin
    Benjamin Calfuqueo
    David Luengo

---
+++ Input.py
@@ -0,0 +1,6 @@
+nombre=input("Ingrese nombre: ")
+apellido=input("Ingrese apellido: ")
+seccion=input("Ingrese su seccion: ")
+sede=input("Ingrese la sede a la que pertenece: ")
+
+print(nombre,apellido,seccion,sede)
---
+++ Print.py
@@ -0,0 +1,4 @@
+print("Evaluación N°1 Programación y Redes Virtualizadas")
+print("Benjamin Calfuqueo")
+print("Kevin san Martin")
+print("David Luengo")
